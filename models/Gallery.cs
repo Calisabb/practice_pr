142c8743b08aaddc16b9cdfdd5280c190990e4e8
Ilham V. Abbasov <ilhamva@code.local>
2026-04-19 14:10:02 +0400

    user1

---
+++ models/Gallery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13.models
+{
+    internal class Gallery
+    {
+        public List<Car> cars { get; set; } = new List<Car>();
+
+        public void AddCar(Car car)
+        { 
+            cars.Add(car);
+        }
+
+        public List<Car> Filter(string brand)
+        {
+            return cars.FindAll(x => x.Brand==brand);
+        }
+        public List<Car> Filter(double mile)
+        {
+            return cars.FindAll(x => x.Mile==mile);
+        }
+
+    }
+}
